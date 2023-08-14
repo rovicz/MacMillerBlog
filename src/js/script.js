5a185fec80e1c8961b9cf442bb21194662d266b3
Victor Fernandes <roviczzf@gmail.com>
2023-08-14 18:59:36 -0300

    dropdown menu 1/?

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
+import dropDown from "./modules/dropDownMenu.js";
 import initAnimacaoScroll from "./modules/scrollAnimation.js";
 import initScrollTop from "./modules/scrollTop.js";
 
 initAnimacaoScroll();
 initScrollTop();
+dropDown();
