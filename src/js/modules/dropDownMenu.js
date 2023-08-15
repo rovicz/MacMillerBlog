660b8b123d9f6cf76f0ae81a36af30095c12b68a
Victor Fernandes <roviczzf@gmail.com>
2023-08-15 17:59:38 -0300

    dropdown menu 1.2

--- src/js/modules/dropDownMenu.js
+++ src/js/modules/dropDownMenu.js
@@ -9,4 +9,18 @@ export default function initDropMenu() {
     dropdownContent.classList.add("show");
     dropdownImg.classList.add("vsgtransform");
   }
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+          dropdownImg.classList.remove("vsgtransform");
+        }
+      }
+    }
+  };
 }
